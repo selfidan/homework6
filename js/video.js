ba6d21174b4e6b9bd01cc53a417009a7ec77bb56
selfidan <70665858+selfidan@users.noreply.github.com>
2020-11-16 15:30:32 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,17 @@
+var video;
+
+window.addEventListener("load", function() {
+	video = document.querySelector("#myVideo");
+	console.log("Starting");
+	console.log(video.volume);
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	
+});
+
