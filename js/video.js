ecb5e0e6429679d36087c1e1fe41d986ec021f48
Selin Fidan <selinfidan@Selins-MBP-6.hsd1.mi.comcast.net>
2020-11-18 16:52:27 -0500

    finished

--- js/video.js
+++ js/video.js
@@ -9,9 +9,63 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
+	let vol = document.querySelector("#volume").innerHTML = video.volume + 99 + "%" 
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	console.log("Pause Video");
+	video.pause();
 });
 
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down Video" + video.playbackRate);
+	//console.log(this.value)
+	video.playbackRate *= .9;
+	//video.playbackRate = video.playbackRate * .9;
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up Video" + video.playbackRate);
+	video.playbackRate /= .9;
+
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute Video");
+	if (video.muted === false) {
+		video.muted = true; 
+	} else { 
+		video.muted = false;
+		
+	}	
+});
+
+document.querySelector("#volumeSlider").addEventListener("click", function() {
+	console.log(this);
+	console.log(this.value);
+	video.volume = this.value / 100;
+	volume.innerHTML = this.value + "%";
+	
+
+});
+
+document.querySelector("#old").addEventListener("click", function(){
+	console.log("Old School Video");
+	video.style.filter = "grayscale(100)";
+	video.style.filter;
+});
+
+document.querySelector("#original").addEventListener("click", function(){
+	console.log("Original Video");
+	video.style.filter = "grayscale(0)";
+	video.style.filter;
+});
+
+
+// document.que for each of the other elements we have to interact with
+
+//(line 23)take that value and use it to update down under the volume bar
+
+//dont change css style directly, you should be using classes and removing classes to get the direired effect
+
+//mute button and skip ahead button for desired effect in other videos (find these videos!!)
